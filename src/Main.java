767def9193c0a418f5ae124a6b6bf8f146ec0e2b
Karol <karolzymkowski1990@gmail.com>
2024-04-17 21:48:11 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,51 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int a = 20;
+        int b = 10;
+        int result = a + b;
+        System.out.println(result);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int c = 20;
+        int d = 10;
+        int result2 = c - d;
+        System.out.println(result2);
+
+        int e = 20;
+        int f = 10;
+        int result3 = e * f;
+        System.out.println(result3);
+
+        int g = 25;
+        int h = 10;
+        double result4 =(double) g / h;
+        System.out.println(result4);
+
+        int i = 20;
+        int j = 0;
+        int result5 = 2 + 2 * 2;
+        System.out.println(result5);
+
+        int k = 20;
+        int l = 10;
+        k += l;
+
+       // k *= l;
+        // k /= l;
+       // k %= l;
+        System.out.println(k);
+
+        int m = 20;
+        int n = 10;
+        m -= n;
+        System.out.println(m);
+
+        int o = 20;
+        int p = 10;
+
+        System.out.println(Math.pow(2, 3));
+
+
     }
-}
+
+    }
